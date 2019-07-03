9bfffa5047aa0270e07586e6594c84ea5b62a5ac
malavikasekhar <42170130+malavikasekhar@users.noreply.github.com>
2019-07-03 16:26:22 +0530

    Create roman.py

---
+++ roman.py
@@ -0,0 +1,5 @@
+a1=input()
+a2=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+for i in range(21):
+  if(a1==a2[i]):
+    print(i)
